e21e9f3f000318329f1136bb0ec446b6448f225f
Alexandr <bazylchik.a@gmail.com>
2021-10-28 00:10:09 +0300

    Realized lobby system with dynamic generic lobbies(unwork overgrow) P.s. now list of lobby is const object array

--- src/components/pages/Main/Logged/index.tsx
+++ src/components/pages/Main/Logged/index.tsx
@@ -1,31 +1,68 @@
 import React from "react"
+import "./styles.scss"
 
 const lobbies = [
   {
     name: "John's lobby",
+    quantity: "2",
     leader: "Benjamin",
     pack: "Hardest ever pack",
     game_token: "1",
   },
   {
     name: "Kate's lobby",
+    quantity: "2",
     leader: "Benjamin",
     pack: "Easy pack",
     game_token: "2",
   },
   {
     name: "Mary's lobby",
+    quantity: "2",
     leader: "Benjamin",
     pack: "Dota 2",
     game_token: "3",
   },
+
 ]
 
 const Logged = () => {
   return (
     <div className="logged_page">
       <div className="lobby_list">
-        sad
+        <div className="lobby_list_header">
+          <p className="lobby_name ">lobby name</p>
+          {/*<p>{lobby.handler}</p>*/}
+          <p className="quantity_">QUA</p>
+          <p className="leader_name">Leader</p>
+          <p className="pack_name">Pack name</p>
+          <p className="connection_button">Connect button</p>
+        </div>
+        <div className="lobby_table">
+          {lobbies.map((lobby) => (
+            <div className="line_splitting">
+              <div className="lobby_name">
+                {lobby.name}
+              </div>
+              <div className="quantity_">
+                {lobby.quantity}
+              </div>
+              <div className="leader_name">
+                {lobby.leader}
+              </div>
+              <div className="pack_name">
+                {lobby.pack}
+              </div>
+              <div className="connection_button">
+                <button onClick = {() => alert(1)}
+                >
+                Connect
+                </button>
+              </div>
+            </div>
+          ))
+          }
+        </div>
       </div>
     </div>
   )
