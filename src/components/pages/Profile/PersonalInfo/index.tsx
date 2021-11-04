19d7056f3b1d72479353d9e7f0cd75c933a593f0
Nikita <gibloor@yandex.ru>
2021-11-04 16:39:40 +0300

    create_base_folders

---
+++ src/components/pages/Profile/PersonalInfo/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+const PersonalInfo = () => {
+  return (
+    <div className="Personal_info">
+      
+    </div>  
+  )
+}
+
+export default PersonalInfo;
--- src/components/pages/Profile/index.tsx
+++ src/components/pages/Profile/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import PersonalInfo from './PersonalInfo';
+import InfoChanger from './InfoChanger';
+import PlayerHistory from './PlayerHistory';
+
 import './styles.scss';
 
 const Profile = () => {
   return (
     <div className="profile">
-
+      <PersonalInfo />
+      <InfoChanger />
+      <PlayerHistory />
     </div>
   )
 }
