c1925757ed406aed37a39ba42366686a4e4ad56d
Alexandr <bazylchik.a@gmail.com>
2021-11-04 17:35:53 +0300

    Initial set up game-page with primitives

---
+++ src/components/pages/Game/Roulette/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./styles.scss";
+import { ReactComponent as RouletteTable } from "assets/table.svg";
+import { ReactComponent as RouletteArrow } from "assets/arrow.svg";
+import { ReactComponent as RouletteTop } from "assets/top_table.svg";
+
+const Roulette = () => {
+  return (
+    <div className="roulette">
+      <div className="roulette_top">
+        <RouletteTop />
+      </div>
+      <div className="roulette_center">
+        <RouletteArrow />
+      </div>
+      <div className="roulette_table">
+        <RouletteTable />
+      </div>
+    </div>
+  );
+};
+
+export default Roulette;
