5af3898169673561ebb0c3270f4edc03edf950d6
Alexandr <bazylchik.a@gmail.com>
2021-11-04 17:36:29 +0300

    Added profile page without any settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,30 @@
-import React from 'react';
-import {Switch, Route} from "react-router-dom";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import Header from './components/Header';
-import Main from './components/pages/Main';
-import Contacts from './components/pages/Contacts';
-import Rights from './components/pages/Rights';
-import Rules from './components/pages/Rules';
-import './App.scss';
-
+import Header from "./components/Header";
+import Main from "./components/pages/Main";
+import Contacts from "./components/pages/Contacts";
+import Rights from "./components/pages/Rights";
+import Rules from "./components/pages/Rules";
+import Lobby from "./components/pages/Lobby";
+import Game from "./components/pages/Game";
+import "./App.scss";
 
 function App() {
-    return (
-        <div className="App">
-            <Header/>
-            <Switch>
-                <Route
-                    exact path="/"
-                    component={() => <Main/>}
-                />
-              <Route
-                path="/contacts"
-                component={() => <Contacts/>}
-              />
-              <Route
-                path="/Rights"
-                component={() => <Rights/>}
-              />
-              <Route
-                path="/Rules"
-                component={() => <Rules/>}
-              />
-            </Switch>
-        </div>
-    );
+  const id = 1;
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={() => <Main />} />
+        <Route path="/contacts" component={() => <Contacts />} />
+        <Route path="/Rights" component={() => <Rights />} />
+        <Route path="/Rules" component={() => <Rules />} />
+        <Route path="/Lobby" component={() => <Lobby />} />
+        <Route path="/Game" component={() => <Game />} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
